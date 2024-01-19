2e886ad75374b0b6b01a273dda89ec4444f57bb7
hAmAruKi <haru.maki.ch.0303@gmail.com>
2024-01-19 16:33:37 +0900

    Update train.py

--- train.py
+++ train.py
@@ -177,6 +177,9 @@ def main(
 
     dataloader_train, dataloader_eval = create_dataloaders(dataset_cfg)
     model = MusicGen.get_pretrained(config["model"])
+    if resume_model_path:
+        model.lm.load_state_dict(torch.load(config["resume_model_path"]))
+
     model.lm = model.lm.to(torch.float32)  # important
 
     scaler = torch.cuda.amp.GradScaler()
